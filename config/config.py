74f53c31fe04880c3b230c22668ac8d631c20281
kentick <49475377+kiolovich888@users.noreply.github.com>
2024-10-01 17:27:16 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -4,8 +4,8 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
-ADMIN_ID = '933411060'
-API_TOKEN = '6841922778:AAG8qdkKO6vE0GHrEOgSNlDLAlwu4h8S_MQ'
+ADMIN_ID = '.....'
+API_TOKEN = '.....'
 DATABASE = 'Anonym_message_users'
 MESSAGE_DATA = 'anonym_message_all'
 QUEUE_DATA = 'users_in_queue'
